f5154767d3e8bb29b6bbdb479c8776a8bf462472
JuniorNovoa1 <87715171+JuniorNovoa1@users.noreply.github.com>
2022-05-04 18:50:44 -0400

    NOOOO

---
+++ source/Startup.hx
@@ -0,0 +1,140 @@
+package;
+
+import lime.app.Application;
+import Discord.DiscordClient;
+import openfl.display.BitmapData;
+import openfl.utils.Assets as OpenFlAssets;
+import flixel.ui.FlxBar;
+import haxe.Exception;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import sys.FileSystem;
+import sys.io.File;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileDiamond;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.transition.TransitionData;
+import flixel.graphics.FlxGraphic;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRect;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import flixel.text.FlxText;
+import flixel.input.keyboard.FlxKey;
+
+using StringTools;
+
+class Startup extends MusicBeatState
+{
+	var toBeDone = 0;
+	var done = 0;
+
+	var loaded = false;
+
+	public static var bitmapData:Map<String, FlxGraphic>;
+
+	var images = [];
+	var music = [];
+	var charts = [];
+
+	override function create()
+	{
+		FlxG.mouse.visible = false;
+
+		var bg:FlxSprite = new FlxSprite();
+		bg.loadGraphic(Paths.image('Loading_screen', 0, 0));
+		add(bg);
+
+		bitmapData = new Map<String, FlxGraphic>();
+
+		#if desktop
+		trace("caching images...");
+
+		// TODO: Refactor this to use OpenFlAssets.
+		for (i in FileSystem.readDirectory(FileSystem.absolutePath("assets/shared/images/characters")))
+		{
+			if (!i.endsWith(".png"))
+				continue;
+			images.push(i);
+		}
+
+		trace("caching music...");
+
+		// TODO: Get the song list from OpenFlAssets.
+		music = Paths.listSongsToCache();
+		#end
+
+		toBeDone = Lambda.count(images) + Lambda.count(music);
+
+		var bar = new FlxBar(10, FlxG.height - 50, FlxBarFillDirection.LEFT_TO_RIGHT, FlxG.width, 40, null, "done", 0, toBeDone);
+		bar.color = FlxColor.PURPLE;
+		add(bar);
+
+		trace('starting caching..');
+
+		#if desktop
+		// cache thread
+		sys.thread.Thread.create(() ->
+		{
+			cache();
+		});
+		#end
+
+		super.create();
+	}
+
+	var calledDone = false;
+
+	override function update(elapsed)
+	{
+		super.update(elapsed);
+	}
+
+	function cache()
+	{
+		#if desktop
+		trace("LOADING: " + toBeDone + " OBJECTS.");
+
+		for (i in images)
+		{
+			var replaced = i.replace(".png", "");
+
+			// var data:BitmapData = BitmapData.fromFile("assets/shared/images/characters/" + i);
+			var imagePath = Paths.image('characters/$i', 'shared');
+			trace('Caching character graphic $i');
+			//var data = OpenFlAssets.getBitmapData(imagePath);
+			//var graph = FlxGraphic.fromBitmapData(data);
+			//graph.persist = true;
+			//bitmapData.set(replaced);
+			done++;
+		}
+
+		for (i in music)
+		{
+			var inst = Paths.inst(i);
+			if (Paths.doesSoundAssetExist(inst))
+			{
+				FlxG.sound.cache(inst);
+			}
+
+			var voices = Paths.voices(i);
+			if (Paths.doesSoundAssetExist(voices))
+			{
+				FlxG.sound.cache(voices);
+			}
+
+			done++;
+		}
+
+		trace("Finished caching...");
+
+		loaded = true;
+
+		trace(OpenFlAssets.cache.hasBitmapData('GF_assets'));
+		#end
+		//LoadingState.loadAndSwitchState(new VideoState("assets/videos/start&end/intro.webm", new TitleState()));
+		FlxG.switchState(new TitleState());
+	}
+}
