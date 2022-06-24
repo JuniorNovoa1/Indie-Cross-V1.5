7212da37cf76ab12a190a96827820926b2a8643f
BushsHaxs <Bushtrain460615@gmail.com>
2022-06-24 08:54:43 -0500

    Crash handler is back
    I stole this from the new version of psych engine

--- source/Main.hx
+++ source/Main.hx
@@ -19,6 +19,7 @@ import sys.FileSystem;
 import sys.io.File;
 import sys.io.Process;
 import GameJolt;
+import Discord.DiscordClient;
 
 class Main extends Sprite
 {
@@ -175,5 +176,44 @@ class Main extends Sprite
 
 		FlxG.autoPause = false;
 		FlxG.mouse.visible = false;
+
+		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
+	}
+	function onCrash(e:UncaughtErrorEvent):Void
+	{
+		var errMsg:String = "";
+		var path:String;
+		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
+		var dateNow:String = Date.now().toString();
+
+		dateNow = dateNow.replace(" ", "_");
+		dateNow = dateNow.replace(":", "'");
+
+		path = "./crash/" + "Indie Cross Error_" + dateNow + ".txt";
+
+		for (stackItem in callStack)
+		{
+			switch (stackItem)
+			{
+				case FilePos(s, file, line, column):
+					errMsg += file + " (line " + line + ")\n";
+				default:
+					Sys.println(stackItem);
+			}
+		}
+
+		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/JuniorNovoa1/Indie-Cross-V1.5\n\n> Crash Handler written by: sqirra-rng";
+
+		if (!FileSystem.exists("./crash/"))
+			FileSystem.createDirectory("./crash/");
+
+		File.saveContent(path, errMsg + "\n");
+
+		Sys.println(errMsg);
+		Sys.println("Crash dump saved in " + Path.normalize(path));
+
+		Application.current.window.alert(errMsg, "Error!");
+		DiscordClient.shutdown();
+		Sys.exit(1);
 	}
 }
